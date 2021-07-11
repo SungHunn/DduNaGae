5fa19d3c42d542d48f3ee3efedff9533a6d7935b
bbbp-park <82454663+bbbp-park@users.noreply.github.com>
2021-07-11 19:44:37 +0900

    test

---
+++ app/src/main/java/com/example/login_example/Matching.java
@@ -0,0 +1,23 @@
+package com.example.login_example;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Matching extends AppCompatActivity {
+
+    Button private_match;
+    Button group_match;
+    Button my_group;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.matching);
+
+
+    }
+}
