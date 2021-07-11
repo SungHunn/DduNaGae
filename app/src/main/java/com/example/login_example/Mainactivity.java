1f5dde1eecc6a04d9395bbec332f820cc72b6e63
LAPTOP-J0GGRB2F\USER <tlgus0113@gmail.com>
2021-07-11 18:55:45 +0900

    Mainactivity.java
    카테고리 클릭 이벤트 생성

--- app/src/main/java/com/example/login_example/Mainactivity.java
+++ app/src/main/java/com/example/login_example/Mainactivity.java
@@ -1,16 +1,36 @@
 package com.example.login_example;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Mainactivity extends AppCompatActivity {
 
-    TextView textView_id;
-    TextView textView_pw;
+
+    TextView category1;
+    TextView category2;
+    TextView category3;
+    TextView category4;
+    TextView category5;
+    TextView category6;
+
+    ImageView today_place1;
+    ImageView today_place2;
+
+    ImageView best_tour1;
+    ImageView best_tour2;
+
+    ImageView best_walk1;
+    ImageView best_walk2;
+
+    ImageView add_menu1;
+    ImageView add_menu2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +49,82 @@ public class Mainactivity extends AppCompatActivity {
         new_animal = getIntent().getStringExtra("ANIMAL_INFO");
         animal_info = getIntent().getStringExtra("ANIMAL_MORE_INFO");
 
-        System.out.println(new_id);
-        System.out.println(new_pw);
-        System.out.println(new_animal);
-        System.out.println(animal_info);
-
-        System.out.println("asdfasdf");
+        onTextViewClick();
     }
+
+    public void onTextViewClick() {
+        category1 = (TextView)findViewById(R.id.category_1);
+        category1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getcategory1;
+                getcategory1 = category1.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), Category.class);
+                intent.putExtra("CATEGORY", getcategory1);
+
+                startActivity(intent);
+            }
+        });
+        category2 = (TextView)findViewById(R.id.category_2);
+        category2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getcategory1;
+                getcategory1 = category2.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), Category.class);
+                intent.putExtra("CATEGORY", getcategory1);
+
+                startActivity(intent);
+            }
+        });
+        category3 = (TextView)findViewById(R.id.category_3);
+        category3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getcategory1;
+                getcategory1 = category3.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), Category.class);
+                intent.putExtra("CATEGORY", getcategory1);
+
+                startActivity(intent);
+            }
+        });
+        category4 = (TextView)findViewById(R.id.category_4);
+        category4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getcategory1;
+                getcategory1 = category4.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), Category.class);
+                intent.putExtra("CATEGORY", getcategory1);
+
+                startActivity(intent);
+            }
+        });
+        category5 = (TextView)findViewById(R.id.category_5);
+        category5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getcategory1;
+                getcategory1 = category5.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), Category.class);
+                intent.putExtra("CATEGORY", getcategory1);
+
+                startActivity(intent);
+            }
+        });
+        category6 = (TextView)findViewById(R.id.category_6);
+        category6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getcategory1;
+                getcategory1 = category6.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), Category.class);
+                intent.putExtra("CATEGORY", getcategory1);
+
+                startActivity(intent);
+            }
+        });
+    }
+
 }
