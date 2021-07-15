6aad8dcbf571de050c71a665c3508807221408c3
LAPTOP-J0GGRB2F\USER <tlgus0113@gmail.com>
2021-07-15 17:14:34 +0900

    Mainactivity.java
    오류 수정

--- app/src/main/java/com/example/Dde_Na_Gae/My_Page.java
+++ app/src/main/java/com/example/Dde_Na_Gae/My_Page.java
@@ -30,6 +30,8 @@ public class My_Page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_page);
 
+
+
         myinfo_page();
         reservation_page();
         my_review_page();
