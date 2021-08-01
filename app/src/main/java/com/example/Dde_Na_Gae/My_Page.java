509d6d076bcd3ae143cd343268fc20d250a5d5aa
yyaallpp <yyaallpp@naver.com>
2021-08-01 15:40:42 +0900

    8/1 ㅇㄷㅌㅇ

--- app/src/main/java/com/example/Dde_Na_Gae/My_Page.java
+++ app/src/main/java/com/example/Dde_Na_Gae/My_Page.java
@@ -3,6 +3,7 @@ package com.example.Dde_Na_Gae;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -39,35 +40,79 @@ public class My_Page extends AppCompatActivity {
         bottomNavigationBar();
         my_picture_change();
 
-        System.out.println("asdfasdf");
     }
 
     private void my_picture_change() {
-        my_picture = findViewById(R.id.my_picture);
-    }
+        my_picture = findViewById(R.id.my_picture); }
 
     private void myinfo_page() {
         my_info = findViewById(R.id.my_info);
+        my_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent my_info_change = new Intent(getApplicationContext(),Mainactivity.class); // Mainactivity말고 회원정보 수정창으로 바꿔야함
+                startActivity(my_info_change);
+            }
+        });
     }
 
     private void reservation_page() {
         reservation = findViewById(R.id.reservation);
+        reservation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent reservation = new Intent(getApplicationContext(),Mainactivity.class); // Mainactivity말고 예약 내약창으로 바꿔야함
+                startActivity(reservation);
+            }
+        });
+
     }
 
     private void my_review_page() {
         my_review = findViewById(R.id.my_review);
+        my_review = findViewById(R.id.my_review);
+        my_review.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent my_review = new Intent(getApplicationContext(),Mainactivity.class); // Mainactivity말고 마이리뷰창으로 바꿔야함
+                startActivity(my_review);
+            }
+        });
     }
 
     private void question_page() {
         question = findViewById(R.id.question);
+        question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent question = new Intent(getApplicationContext(),Mainactivity.class); // Mainactivity말고 고객센터로 바꿔야함
+                startActivity(question);
+            }
+        });
     }
 
     private void kakao_question_page() {
         kakao_question = findViewById(R.id.kakao_question);
+        kakao_question = findViewById(R.id.kakao_question);
+        kakao_question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent kakao_question = new Intent(getApplicationContext(),Mainactivity.class); // Mainactivity말고 카카오 문의로 바꿔야함
+                startActivity(kakao_question);
+            }
+        });
     }
 
     private void affiliate_page() {
         affiliate = findViewById(R.id.affiliate);
+        affiliate = findViewById(R.id.affiliate);
+        affiliate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent affiliate = new Intent(getApplicationContext(),Mainactivity.class); // Mainactivity말고 제휴연결로 바꿔야함
+                startActivity(affiliate);
+            }
+        });
     }
 
     private void bottomNavigationBar() {
--- app/src/main/java/com/example/Dde_Na_Gae/Notice.java
+++ app/src/main/java/com/example/Dde_Na_Gae/Notice.java
@@ -1,12 +1,17 @@
 package com.example.Dde_Na_Gae;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Notice extends AppCompatActivity {
 
