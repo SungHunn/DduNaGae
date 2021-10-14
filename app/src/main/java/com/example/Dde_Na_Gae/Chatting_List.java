f01835784b4034f95a4c8210525d030e0e395db6
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2021-10-14 23:48:17 +0900

    뒤로가기 수정

--- app/src/main/java/com/example/Dde_Na_Gae/Chatting_List.java
+++ app/src/main/java/com/example/Dde_Na_Gae/Chatting_List.java
@@ -2,6 +2,8 @@ package com.example.Dde_Na_Gae;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
@@ -9,15 +11,27 @@ import androidx.fragment.app.FragmentTransaction;
 import com.example.Dde_Na_Gae.fragment.RoomFragment;
 public class Chatting_List extends AppCompatActivity {
 
+    private ImageView go_back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chatting_list);
 
+        go_back = (ImageView)findViewById(R.id.matching_roomlist_img_back);
+        go_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), New_ChatMainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
         RoomFragment roomFragment = new RoomFragment();
 
-    Intent getintent = getIntent();
-    String chatting_room_option_selector = getintent.getExtras().getString("chatting_room_option_selector");
+       Intent getintent = getIntent();
+       String chatting_room_option_selector = getintent.getExtras().getString("chatting_room_option_selector");
 
        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
        Bundle bundle = new Bundle();
